f09377756cd50dc8c9cd2596665e2a6675093d50
Elyablegese <101351707+Elyablegese@users.noreply.github.com>
2022-11-29 16:15:54 +0300

    new walla

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,52 +1,52 @@
 #!/usr/bin/python3
-"""
-module for function to parses the title of all hot articles, and prints
-a sorted count of given keywords (case-insensitive, delimited by spaces
-"""
+"""Top ten"""
+import requests
 
 
-def count_words(subreddit, word_list, instances={}, count=0, after=None):
-    """
-    Args:
-        subreddit: subreddit name
-        word_list: a list containing the keywords of all hot articles titles
-        instances: a dictionary containing the keywords and their frequency
-        count: counter to change prototype
-        after: to validate subreddit
-
-    Returns:
-        returns a dictionary containing the frequency of certain
-        keywords in the hot articles
-    """
-    import requests
-
-    headers = {'User-Agent': 'User-Agent'}
-    parameters = {'after': after,
-                  'count': count}
+def count_words(subreddit, word_list, after="", word_dic={}):
+    """Return a list of titles of all hot articles"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(url, headers=headers, params=parameters,
-                            allow_redirects=False)
-    if response.status_code == 200:
-        results = response.json().get("data")
-        after = results.get("after")
-        count += results.get("dist")
-        for child in results.get("children"):
-            hot_list = child.get("data").get("title").lower().split()
-            for word in word_list:
-                if word.lower() in hot_list:
-                    times = len([article for article in hot_list if
-                                article == word.lower()])
-                    if instances.get(word.lower()) is None:
-                        instances[word.lower()] = times
-                    else:
-                        instances[word.lower()] += times
+    headers = {'user-agent': 'MyAPI/0.0.1'}
+    params = {'after': after}
 
-        if after is None:
-            if len(instances) == 0:
-                return
-            instances = sorted(instances.items(),
-                               key=lambda kv: (-kv[1], kv[0]))
-            [print("{}: {}".format(k, v)) for k, v in instances]
-        else:
-            count_words(subreddit, word_list, instances, count, after)
-    return
+    if not word_dic:
+        for word in word_list:
+            word_dic[word] = 0
+
+    if after is None:
+        word_list = [[key, value] for key, value in word_dic.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+        for w in word_list:
+            if w[1]:
+                print("{}: {}".format(w[0].lower(), w[1]))
+        return None
+
+    r = requests.get(url, headers=headers, params=params,
+                     allow_redirects=False)
+
+    if r.status_code != 200:
+        return None
+
+    try:
+        js = r.json()
+
+    except ValueError:
+        return None
+
+    try:
+
+        data = js.get("data")
+        after = data.get("after")
+        children = data.get("children")
+        for child in children:
+            post = child.get("data")
+            title = post.get("title")
+            lower = [s.lower() for s in title.split(' ')]
+
+            for w in word_list:
+                word_dic[w] += lower.count(w.lower())
+
+    except Exception as e:
+        return None
+
+    count_words(subreddit, word_list, after, word_dic)
